787ec08762746c42f9979e927c604609714a808a
Velshh <pkot2003@gmail.com>
2024-09-13 22:12:10 +0200

    change return type of wordsList() method to ResponseEntity

--- src/main/java/me/mmtr/wordsapi/controller/WordsController.java
+++ src/main/java/me/mmtr/wordsapi/controller/WordsController.java
@@ -3,6 +3,8 @@ package me.mmtr.wordsapi.controller;
 import me.mmtr.wordsapi.record.Words;
 import me.mmtr.wordsapi.service.WordsService;
 import me.mmtr.wordsapi.util.WordsList;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,11 +24,11 @@ public class WordsController {
     }
 
     @GetMapping("/get")
-    public Words wordsList(@RequestParam(required = false) Integer length,
-                           @RequestParam(required = false) CharSequence containing,
-                           @RequestParam(required = false) String starting,
-                           @RequestParam(required = false) String ending,
-                           @RequestParam(required = false, defaultValue = "1") Integer amount) {
+    public ResponseEntity<?> wordsList(@RequestParam(required = false) Integer length,
+                                       @RequestParam(required = false) CharSequence containing,
+                                       @RequestParam(required = false) String starting,
+                                       @RequestParam(required = false) String ending,
+                                       @RequestParam(required = false, defaultValue = "1") Integer amount) {
         WordsList.Builder wordsListBuilder = WORDS_SERVICE.buildWordsList();
 
         if (length != null && length > 0) {
@@ -48,6 +50,6 @@ public class WordsController {
         if (amount != null && amount > 0) {
             wordsListBuilder.amount(amount);
         }
-        return new Words(ATOMIC_LONG.incrementAndGet(), wordsListBuilder.build().getWordsList());
+        return new ResponseEntity<>(new Words(ATOMIC_LONG.incrementAndGet(), wordsListBuilder.build().getWordsList()), HttpStatus.OK);
     }
 }
